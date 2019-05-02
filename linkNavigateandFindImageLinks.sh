fe44eadd2af2136a57b16faf54fa8434ba26973e
Kaushik <davidkgp@gmail.com>
2019-05-02 23:17:53 +0200

    WIP for adding depth

--- linkNavigateandFindImageLinks.sh
+++ linkNavigateandFindImageLinks.sh
@@ -5,11 +5,14 @@ source ./util/util.sh
 
 ROOTLINK=$1
 
+DEPTH=${2//[!0-9]/}
+CURRENT_DEPTH=${3//[!0-9]/}
 
 #echo $(alreadVisited $ROOTLINK)
 
-if [[ $(alreadVisited $ROOTLINK) == "no" ]]; then
+if [[ $(alreadVisited $ROOTLINK) == "no" && $(( CURRENT_DEPTH == DEPTH+1 )) == 0 ]]; then
 
+  
 
 	addToVisited $ROOTLINK
 	createImgFolder
@@ -28,6 +31,8 @@ if [[ $(alreadVisited $ROOTLINK) == "no" ]]; then
       if [[ $(hrefisAbsolute $link) == "yes" ]]; then
       	 LINK=${link##*=}
   	     LINK_WITHOUTQUOTES=$(getStringinBetweenDoubleQuotes $LINK)
+
+         #echo "$LINK_WITHOUTQUOTES DEPTH:$CURRENT_DEPTH"
   	    
 
 
@@ -35,7 +40,7 @@ if [[ $(alreadVisited $ROOTLINK) == "no" ]]; then
 
         	#echo "$(isImgLink $LINK_WITHOUTQUOTES) : $LINK_WITHOUTQUOTES"
         	
-        		linkNavigateandFindImageLinks.sh $LINK_WITHOUTQUOTES
+        		linkNavigateandFindImageLinks.sh $LINK_WITHOUTQUOTES $DEPTH $(( CURRENT_DEPTH+1 ))
         	
         else
         	  silentlog "Image download $LINK_WITHOUTQUOTES"
@@ -48,13 +53,15 @@ if [[ $(alreadVisited $ROOTLINK) == "no" ]]; then
   	     PART_LINK=${link##*=}
   	     PART_LINK_WITHOUTQUOTES=$(getStringinBetweenDoubleQuotes $PART_LINK)
   	     FINAL_LINK=$DOMAIN_URL$PART_LINK_WITHOUTQUOTES
+
+         #echo "$FINAL_LINK DEPTH:$CURRENT_DEPTH"
   	     #echo $FINAL_LINK
 
          if [[ $(isImgLink $FINAL_LINK) == "no" ]]; then
 
          	    #echo "$(isImgLink $FINAL_LINK) : $FINAL_LINK"
         	
-        		 linkNavigateandFindImageLinks.sh $FINAL_LINK
+        		 linkNavigateandFindImageLinks.sh $FINAL_LINK $DEPTH $(( CURRENT_DEPTH+1 ))
         	
          else
          	silentlog "Image download $FINAL_LINK"
--- spider.sh
+++ spider.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 rm -rf "$WORKINGDIR/tmp"
 rm -rf "$WORKINGDIR/img"
-linkNavigateandFindImageLinks.sh $1
+
+if [ "$#" -ne 2 ]; then
+   echo -e "\e[31m Illegal number of parameters,pass in the link and the depth to be scrapped respectively \e[0m"
+   exit 1
+fi
+
+DEPTH=$2
+
+VALUE=${DEPTH//[!0-9]/}
+
+
+
+if [ $(( VALUE <= 1 )) == 1 ]; then
+   echo -e "\e[31m Depth of traversal has to be more than 1 \e[0m"
+   exit 1
+fi
+
+
+
+linkNavigateandFindImageLinks.sh $1 $DEPTH 0
